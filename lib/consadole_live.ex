fdc0282e8c50524148fe55bbe1aaee180cfd567a
niku <niku@niku.name>
2015-07-26 17:05:15 +0900

    Fetch from flash text

--- lib/consadole_live.ex
+++ lib/consadole_live.ex
@@ -13,12 +13,30 @@ defmodule ConsadoleLive do
     end
     defp process({_parsed, argv}) do
       case argv do
-        [x] -> IO.inspect(x)
+        [x] -> ConsadoleLive.run(x)
         _ ->
           Logger.error "Can't accept argv: #{inspect argv}"
           IO.puts ~s(You have to pass only one argument like: "./consadole_live 596")
           System.halt(1)
       end
     end
+  end
+
+  def run(number) do
+    build_uri(number)
+    |> fetch
+    |> parse
+  end
+
+  def build_uri(number), do: URI.parse("http://www.consadole-sapporo.jp/game/flash_text/?mid=#{number}")
+  def fetch(uri), do: HTTPoison.get!(uri) |> Map.get(:body)
+  def parse(doc) do
+    Floki.find(doc, "article table tr")
+    |> Enum.map(&Floki.find(&1, "td"))
+    |> Enum.map(&Enum.map(&1, fn(x) -> Floki.text(x) end))
+    |> Enum.map(fn
+         [a]    -> {"",            Floki.text(a)}
+         [a, b] -> {Floki.text(a), Floki.text(b)}
+       end)
   end
 end
