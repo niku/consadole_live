10e546e884a26ae2836f9b7e735ad432c181937a
niku <niku@niku.name>
2015-07-26 23:36:07 +0900

    Extract module CLI

--- lib/consadole_live.ex
+++ lib/consadole_live.ex
@@ -1,27 +1,4 @@
 defmodule ConsadoleLive do
-  require Logger
-
-  defmodule CLI do
-    def main(argv), do: argv |> parse_args |> process
-    defp parse_args(args) do
-      {parsed, argv, errors} = OptionParser.parse(args, strict: [])
-      Logger.info "Parse ARGV to parsed: #{inspect parsed}, argv: #{inspect argv}, errors: #{inspect errors}"
-      unless Enum.empty?(errors) do
-        Logger.warn "Invalid ARGV: #{inspect errors}"
-      end
-      {parsed, argv}
-    end
-    defp process({_parsed, argv}) do
-      case argv do
-        [x] -> ConsadoleLive.run(x)
-        _ ->
-          Logger.error "Can't accept argv: #{inspect argv}"
-          IO.puts ~s(You have to pass only one argument like: "./consadole_live 596")
-          System.halt(1)
-      end
-    end
-  end
-
   def run(number) do
     build_uri(number)
     |> fetch
---
+++ lib/consadole_live/cli.ex
@@ -0,0 +1,22 @@
+defmodule ConsadoleLive.CLI do
+  require Logger
+
+  def main(argv), do: argv |> parse_args |> process
+  defp parse_args(args) do
+    {parsed, argv, errors} = OptionParser.parse(args, strict: [])
+    Logger.info "Parse ARGV to parsed: #{inspect parsed}, argv: #{inspect argv}, errors: #{inspect errors}"
+    unless Enum.empty?(errors) do
+      Logger.warn "Invalid ARGV: #{inspect errors}"
+    end
+    {parsed, argv}
+  end
+  defp process({_parsed, argv}) do
+    case argv do
+      [x] -> ConsadoleLive.run(x)
+      _ ->
+        Logger.error "Can't accept argv: #{inspect argv}"
+        IO.puts ~s(You have to pass only one argument like: "./consadole_live 596")
+        System.halt(1)
+    end
+  end
+end
