39f39c3d5007c3e1b394001522d1f6b07b48f975
niku <niku@niku.name>
2015-07-29 20:01:34 +0900

    Implement ConsadoleLive

--- lib/consadole_live.ex
+++ lib/consadole_live.ex
@@ -1,12 +1,17 @@
 defmodule ConsadoleLive do
-  def run(number) do
-    build_uri(number)
-    |> fetch
-    |> parse
+  require Logger
+
+  def run(number, twitter_config) do
+    {:ok, twitter_pid} = ConsadoleLive.Twitter.start_link(twitter_config)
+    uri = build_uri(number)
+    Logger.info("ConsadoleLive.run uri: #{uri}")
+
+    execute(twitter_pid, uri, 0, hash(""))
   end
 
   def build_uri(number), do: URI.parse("http://www.consadole-sapporo.jp/game/flash_text/?mid=#{number}")
   def fetch(uri), do: HTTPoison.get!(uri) |> Map.get(:body)
+  def hash(doc), do: :crypto.hash(:md5, doc)
   def parse(doc) do
     Floki.find(doc, "article table tr")
     |> Enum.map(&Floki.find(&1, "td"))
@@ -15,5 +20,23 @@ defmodule ConsadoleLive do
          [a]    -> {"",            Floki.text(a)}
          [a, b] -> {Floki.text(a), Floki.text(b)}
        end)
+  end
+
+  defp execute(_twitter_pid, _uri, dup_times, _hash) when 9 < dup_times do
+    Logger.info("Exit, because document has no any changes.")
+    exit(0)
+  end
+  defp execute(twitter_pid, uri, dup_times, hash) do
+    Logger.info("ConsadoleLive.execute dup_times: #{dup_times}, hash: #{hash}")
+    doc = fetch(uri)
+    case hash(doc) do
+      ^hash ->
+        :timer.sleep(1000 * 60) # sleep 60 seconds
+        execute(twitter_pid, uri, dup_times + 1, hash)
+      new_hash ->
+        Task.start(Consadole.Twitter, :post, parse(doc))
+        :timer.sleep(1000 * 60) # sleep 60 seconds
+        execute(twitter_pid, uri, 0, new_hash)
+    end
   end
 end
--- lib/consadole_live/cli.ex
+++ lib/consadole_live/cli.ex
@@ -11,8 +11,12 @@ defmodule ConsadoleLive.CLI do
     {parsed, argv}
   end
   defp process({_parsed, argv}) do
+    twitter_config = %{consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+                       consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+                       access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
+                       access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")}
     case argv do
-      [x] -> ConsadoleLive.run(x)
+      [x] -> ConsadoleLive.run(x, twitter_config)
       _ ->
         Logger.error "Can't accept argv: #{inspect argv}"
         IO.puts ~s(You have to pass only one argument like: "./consadole_live 596")
